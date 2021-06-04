8c04c12a9cb5d89b1c5264c5bd8db86cf84a827f
BTK <45247934+b-t-k@users.noreply.github.com>
2021-06-03 19:27:15 -0600

    Initial Commit

---
+++ list_python_source.py
@@ -0,0 +1,24 @@
+import math
+import os
+import sys
+
+import requests
+
+
+name = input("your name?")
+print("hello", name)
+
+
+def greeting(who):
+    greeting = "hey {}".format(who)
+    return greeting
+
+
+#  print (sys.version)
+print(sys.executable)
+
+r = requests.get("https://macblaze.ca")
+
+print(r.status_code)
+
+# shft opt f = autoformat
